11606ae89e09b3534f09cc532ae30e1d19d02481
Jared Huffstutler <jaredhuff85@gmail.com>
2020-03-18 19:33:33 -0500

    Updates

---
+++ precache-manifest.6744aa529bc4a99612147eb8c2b1830b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e35613a8151dfa951ad8f1c47705055b",
+    "url": "/retail-info/index.html"
+  },
+  {
+    "revision": "da7f23480ac104c0a5ec",
+    "url": "/retail-info/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "8d21e799e0ab281ea960",
+    "url": "/retail-info/static/css/main.676efa82.chunk.css"
+  },
+  {
+    "revision": "da7f23480ac104c0a5ec",
+    "url": "/retail-info/static/js/2.701858ef.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/retail-info/static/js/2.701858ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d21e799e0ab281ea960",
+    "url": "/retail-info/static/js/main.55df5df6.chunk.js"
+  },
+  {
+    "revision": "fbb537478382bad64299",
+    "url": "/retail-info/static/js/runtime-main.a8bbc453.js"
+  }
+]);
