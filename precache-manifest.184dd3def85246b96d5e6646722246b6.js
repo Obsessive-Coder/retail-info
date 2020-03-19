843f81c1ee59c7eb1636562d4018dbdff355cf38
Jared Huffstutler <jaredhuff85@gmail.com>
2020-03-19 17:57:02 -0500

    Updates

--- precache-manifest.184dd3def85246b96d5e6646722246b6.js
+++ precache-manifest.184dd3def85246b96d5e6646722246b6.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3f3cfd652332566e5958dd78383969fa",
+    "revision": "2a0e30392cac11e4e92c711fd5b573e6",
     "url": "/retail-info/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/retail-info/static/css/2.17e5ed98.chunk.css"
   },
   {
-    "revision": "7b312dfeeb559a02e046",
+    "revision": "b208816be01152e3ce2c",
     "url": "/retail-info/static/css/main.6da0e833.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/retail-info/static/js/2.c48353b5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7b312dfeeb559a02e046",
-    "url": "/retail-info/static/js/main.5d2caf5f.chunk.js"
+    "revision": "b208816be01152e3ce2c",
+    "url": "/retail-info/static/js/main.a6168e74.chunk.js"
   },
   {
     "revision": "fbb537478382bad64299",
