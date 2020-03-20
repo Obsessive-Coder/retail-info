d3e2a50dba57d81836d4d6aa6d4c36878ba7fbbe
Jared Huffstutler <jaredhuff85@gmail.com>
2020-03-19 23:09:01 -0500

    added images and mapsSigned-off-by: Jared Huffstutler <jaredhuff85@gmail.com>
    Signed-off-by: Jared Huffstutler <jaredhuff85@gmail.com>

--- src/components/BusinessDetails.js
+++ src/components/BusinessDetails.js
@@ -4,12 +4,13 @@ import { Link } from 'react-router-dom';
 import { Card, CardTitle, CardText } from 'reactstrap';
 import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Map, Marker , GoogleApiWrapper } from 'google-maps-react';
 
 // Restaurant Data File.
 const businessData = require('../data/businesses.json')
 const menuData = require('../data/menus.json');
 
-export default class BusinessDetails extends Component {
+export class BusinessDetails extends Component {
   constructor(props) {
     super(props);
 
@@ -23,14 +24,14 @@ export default class BusinessDetails extends Component {
   }
 
   handleAddressClick() {
-    const { business: { address } } = this.state;
+    const { business: { name, address } } = this.state;
 
-    if ((navigator.platform.indexOf("iPhone") != -1) ||
-     (navigator.platform.indexOf("iPad") != -1) ||
-     (navigator.platform.indexOf("iPod") != -1)) {
-      window.open(`maps://maps.google.com/maps/dir/?daddr=${address}&amp;ll=`);
+    if ((navigator.platform.indexOf("iPhone") !== -1) ||
+     (navigator.platform.indexOf("iPad") !== -1) ||
+     (navigator.platform.indexOf("iPod") !== -1)) {
+      window.open(`maps://maps.google.com/maps/dir/?daddr=${name}%20${address}&amp;ll=`);
     } else {
-      window.open(`https://maps.google.com/maps/dir/?daddr=${address}&amp;ll=`);
+      window.open(`https://maps.google.com/maps/dir/?daddr=${name}%20${address}&amp;ll=`);
     }
   }
 
@@ -39,12 +40,40 @@ export default class BusinessDetails extends Component {
     const { menuId } = this.props.match.params;
     const menuItems = menuData[menuId] || [];
     const business = businessData.businesses.filter(business => business.menuId === menuId)[0];
+    const { name, address } = business;
 
-    this.setState(() => ({ business, menuItems }))
+    const { google } = this.props;
+    const map = new google.maps.Map(<div></div>);
+
+    var service = new google.maps.places.PlacesService(map);
+
+    const request = {
+      query: `${name} ${address}`,
+      fields: ['photos', 'geometry'],
+    };
+
+    service.findPlaceFromQuery(request, (results, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          const {
+            geometry: { location: { lat, lng }},
+            photos: [ previewImage ],
+          } = results[i];
+          business.image = previewImage.getUrl();
+          business.location = {
+            lat: lat(),
+            lng: lng(),
+          };
+        }
+
+        this.setState(() => ({ business, menuItems }));
+      }
+    });
   }
 
   render() {
     const { business, menuItems } = this.state;
+    const { google } = this.props;
 
     if (!business) return null;
 
@@ -53,6 +82,8 @@ export default class BusinessDetails extends Component {
       address,
       phone,
       hours,
+      image,
+      location,
     } = business;
 
     const booleanFields = [{
@@ -74,53 +105,77 @@ export default class BusinessDetails extends Component {
         <h1 className="py-3 bg-dark text-light text-center page-heading">
           Restaurants and Bars
         </h1>
-        <div className="d-flex flex-column flex-md-row mt-4">
-          <div className="mb-4 px-4 mb-md-0 border-right border-bottom">
-            <h2 className="mb-0 text-center text-dark business-heading">
-              {name}
-            </h2>
-            {/* <p className="mb-0 text-center text-secondary">{address}</p> */}
-            <Link
-              onClick={this.handleAddressClick}
-              className="mx-2"
-            >
-              {address}
-            </Link>
-            <div className="d-sm-flex flex-md-column justify-content-sm-around text-center text-secondary">
-              <div className="d-flex justify-content-around font-lg">
-                <span className="font-weight-bold">Phone:</span>
-                <a
-                  href={`tel:${phone}`}
-                  className="mx-2"
-                >
-                  {phone}
-                </a>
-              </div>
-              <div className="d-flex justify-content-around font-lg">
-                <span className="font-weight-bold">Hours:</span>
-                <span className="mx-2 text-capitalize">{hours}</span>
-              </div>
-            </div>
-            <div className="d-flex my-3 text-secondary">
-              {booleanFields.map(({ propertyName, labelText }) => (
-                <div
-                  key={propertyName}
-                  className="d-flex flex-column align-items-center flex-fill text-center"
-                >
-                  <FontAwesomeIcon
-                    fixedWidth
-                    size="2x"
-                    icon={business[propertyName] ? faCheck : faTimes}
-                    className={business[propertyName] ? 'text-success' : 'text-danger'}
-                  />
-                  <span className="font-weight-bold font-sm">
-                    {labelText}
-                  </span>
+        <div className="d-flex flex-column flex-md-row">
+          <div className="mb-4 px-1 pb-3 mb-md-0 border-right border-bottom details-sidebar">
+            <img
+              src={image}
+              alt={name}
+              className="w-100 h-auto business-details-image"
+            />
+            <div className="p-3">
+              <h2 className="mb-0 text-center text-dark business-heading">
+                {name}
+              </h2>
+              <Link
+                to="#"
+                onClick={this.handleAddressClick}
+                className="mx-2"
+              >
+                {address}
+              </Link>
+              <div className="d-sm-flex flex-md-column justify-content-sm-around text-center text-secondary">
+                <div className="d-flex justify-content-around font-lg">
+                  <span className="font-weight-bold">Phone:</span>
+                  <a
+                    href={`tel:${phone}`}
+                    className="mx-2"
+                  >
+                    {phone}
+                  </a>
                 </div>
-              ))}
+                <div className="d-flex justify-content-around font-lg">
+                  <span className="font-weight-bold">Hours:</span>
+                  <span className="mx-2 text-capitalize">{hours}</span>
+                </div>
+              </div>
+              <div className="d-flex my-3 text-secondary">
+                {booleanFields.map(({ propertyName, labelText }) => (
+                  <div
+                    key={propertyName}
+                    className="d-flex flex-column align-items-center flex-fill text-center"
+                  >
+                    <FontAwesomeIcon
+                      fixedWidth
+                      size="2x"
+                      icon={business[propertyName] ? faCheck : faTimes}
+                      className={business[propertyName] ? 'text-success' : 'text-danger'}
+                    />
+                    <span className="font-weight-bold font-sm">
+                      {labelText}
+                    </span>
+                  </div>
+                ))}
+              </div>
             </div>
+
+            {menuItems.length > 0 && (
+              <div className="px-4 px-sm-5 px-md-0">
+                <Map
+                  google={google}
+                  zoom={16}
+                  initialCenter={location}
+                  style={{ position: 'relative',  width: '100%', height: '200px' }}
+                  className="position-relative"
+                >
+                  <Marker
+                    position={location}
+                    onClick={this.handleAddressClick}
+                  />
+                </Map>
+              </div>
+            )}
           </div>
-          <div className="flex-fill px-4">
+          <div className="flex-fill px-4 py-2">
             <h3 className="text-center border-bottom menu-heading">Menu</h3>
 
             {menuItems.length === 0 && (
@@ -136,6 +191,21 @@ export default class BusinessDetails extends Component {
                   <a href="mailto:jaredhuff85@gmail.com?Subject=Retail%20Info">Jared</a>
                   .
                 </p>
+
+                <div>
+                  <Map
+                    google={google}
+                    zoom={16}
+                    initialCenter={location}
+                    style={{ position: 'relative',  width: '100%', height: '350px' }}
+                    className="position-relative"
+                  >
+                    <Marker
+                      position={location}
+                      onClick={this.handleAddressClick}
+                    />
+                  </Map>
+                </div>
               </div>
             )}
 
@@ -164,3 +234,7 @@ export default class BusinessDetails extends Component {
     );
   }
 }
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBWc45Eb4D3pf2A-I3aki-aM8HxMHRPfpc'
+})(BusinessDetails);
--- src/components/BusinessList.js
+++ src/components/BusinessList.js
@@ -1,53 +1,31 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
 
-import {
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-} from 'reactstrap';
-import { BusinessTable, BusinessGrid } from './';
+import { BusinessGrid } from './';
 
 // Restaurant Data File.
-const dataFile = require('../data/businesses.json');
+const { businesses } = require('../data/businesses.json');
 
 export default class BusinessList extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      businesses: dataFile.businesses || [],
+      businesses: businesses || [],
     };
 
     // Bind class methods.
     this.handleAddressClick = this.handleAddressClick.bind(this);
   }
 
-  handleAddressClick(address) {
-    if ((navigator.platform.indexOf("iPhone") != -1) ||
-     (navigator.platform.indexOf("iPad") != -1) ||
-     (navigator.platform.indexOf("iPod") != -1)) {
-      window.open(`maps://maps.google.com/maps/dir/?daddr=${address}&amp;ll=`);
+  handleAddressClick(name, address) {
+    if ((navigator.platform.indexOf("iPhone") !== -1) ||
+     (navigator.platform.indexOf("iPad") !== -1) ||
+     (navigator.platform.indexOf("iPod") !== -1)) {
+      window.open(`maps://maps.google.com/maps/dir/?daddr=${name}%20${address}&amp;ll=`);
     } else {
-      window.open(`https://maps.google.com/maps/dir/?daddr=${address}&amp;ll=`);
+      window.open(`https://maps.google.com/maps/dir/?daddr=${name}%20${address}&amp;ll=`);
     }
   }
-
-  // componentDidMount() {
-    // https://maps.googleapis.com/maps/api/place/textsearch/json?query=123+main+street&key=AIzaSyBWc45Eb4D3pf2A-I3aki-aM8HxMHRPfpc
-    // axios.get('https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=alfanos+oregon+il&key=AIzaSyBWc45Eb4D3pf2A-I3aki-aM8HxMHRPfpc')
-    //   .then(result => {
-        // CmRaAAAAOmfNZQhEKPzYNWHmtbmLg5Kd7guIP2Pp_QsFd8E3cUH2UewiZHsMJAizWJmYVq8_jfFofTsMFpK-92G_27ZowH7y4MjwyBQJkrLhu9meCJbyRvT1rQSbkjTAw4yvzGAWEhAHoq0pPnjJqjnPhsvd4v74GhRZ7gwkZnGQNWsOWM0Ao97ztYyUzw
-        // console.log(result.data);
-
-        // axios.get('https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU&key=AIzaSyBWc45Eb4D3pf2A-I3aki-aM8HxMHRPfpc')
-        // .then(resultX => {
-        //   console.log(resultX.data);
-        // })
-      // })
-      // .catch(error => console.log(error));
-  // }
 
   render() {
     const { businesses } = this.state;
@@ -59,7 +37,10 @@ export default class BusinessList extends Component {
         </h1>
 
         <div className="mx-2">
-          <BusinessGrid businesses={businesses} handleAddressClick={this.handleAddressClick} />
+          <BusinessGrid
+            businesses={businesses}
+            handleAddressClick={this.handleAddressClick}
+          />
         </div>
       </div>
     )
