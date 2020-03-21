137eb4c5e5ee6a8bd9a98977499d6cc4a73dbd96
Jared Huffstutler <jaredhuff85@gmail.com>
2020-03-21 13:15:32 -0500

    added selected city to dropdown
    Signed-off-by: Jared Huffstutler <jaredhuff85@gmail.com>

--- src/components/CityDropdown.js
+++ src/components/CityDropdown.js
@@ -8,6 +8,7 @@ export default function CityDropdown({
   cities, handleLocationItemOnClick
 }) {
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedLocation, setSelectedLocation] = useState('all');
 
   return (
     <Dropdown isOpen={isOpen} toggle={() => setIsOpen(!isOpen)}>
@@ -15,14 +16,17 @@ export default function CityDropdown({
         caret
         outline
         color="info"
-        className="mx-2"
+        className="mx-2 text-capitalize"
       >
-        Location
+        <span className="mx-2">{selectedLocation}</span>
       </DropdownToggle>
       <DropdownMenu>
         {cities.map(city => (
           <DropdownItem
-            onClick={() => handleLocationItemOnClick(city)}
+            onClick={() => {
+              setSelectedLocation(city);
+              handleLocationItemOnClick(city);
+            }}
             className="text-capitalize"
           >
             {city}
