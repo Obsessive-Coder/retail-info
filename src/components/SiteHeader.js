34c98f88a4bfb273d036a26ec6a7a5dced4718f0
Jared Huffstutler <jaredhuff85@gmail.com>
2020-03-21 06:04:15 -0500

    fixing mobile font sizes
    Signed-off-by: Jared Huffstutler <jaredhuff85@gmail.com>

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -18,26 +18,20 @@ export default function SiteHeader() {
     <Navbar
       dark
       color="dark"
-      className="navbar-expand-sm py-0 border-bottom page-header"
+      className="py-0 border-bottom page-header"
     >
-      <NavbarBrand href="/" className="mr-auto font-xxl">
+      <NavbarBrand tag={Link} to="/" className="mr-auto font-xxl">
         Restaurants and Bars
       </NavbarBrand>
-      <NavbarToggler onClick={toggleIsOpen} className="mr-2" />
-      <Collapse
-        navbar
-        isOpen={!isOpen}
-        className="align-self-end font-lg"
-      >
-        <Nav navbar className="flex-row justify-content-around ml-auto">
-          <NavItem>
-            <NavLink tag={Link} to="/">Home</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={Link} to="/map">Map</NavLink>
-          </NavItem>
-        </Nav>
-      </Collapse>
+
+      <Nav navbar className="flex-row justify-content-around ml-auto font-xl">
+        <NavItem className="mx-3">
+          <NavLink tag={Link} to="/">Home</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink tag={Link} to="/map">Map</NavLink>
+        </NavItem>
+      </Nav>
     </Navbar>
   );
 }
